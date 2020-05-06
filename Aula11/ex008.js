8fbc5e3346704f029591c01d1bdebe38abcb151e
artvinicius <arthurviny1999@gmail.com>
2020-05-06 15:56:00 -0300

    Aula11 Condições

---
+++ Aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+
+if(vel > 60){ //Condição simples 
+    console.log('Vocẽ ultrapassou a velocidade permitida. MULTADO !')
+}
+
+console.log('Digija sempre com cinto de segurança')
---
+++ Aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = "Brasil"
+if (pais == "Brasil"){
+    console.log('Você é Basileiro')
+} else {
+    console.log('Você Estrangeiro!')
+}
